32adc3ad76ea1e7231c05bb7001c206877216ec1
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-02-15 22:00:52 -0800

    Chore: Created a settings module

---
+++ settings.py
@@ -0,0 +1,10 @@
+'''A module that stores the settings for Alien Invasion'''
+
+class Settings:
+    '''A class to store all the settings for Alien Invasion'''
+    def __init__(self):
+        '''Initialize the game's settings.'''
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
