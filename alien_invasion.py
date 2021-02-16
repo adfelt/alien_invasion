8d014f0ad6b8c60801e860a27a072f5cf634ec1f
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-02-15 22:09:09 -0800

    Refactor: Update settings in game file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from settings import Settings
 import pygame
 
 class AlienInvasion:
@@ -6,8 +7,10 @@ class AlienInvasion:
     def __init__(self):
         '''Initialize the game, and create game resources.'''
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -22,7 +25,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most erecently drawn screen visible.
             pygame.display.flip()
