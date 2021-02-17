6625405291fa2ceb12b8ed83c02fdda57dd5b4e8
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-02-16 21:17:12 -0800

    Fix: Bug fix for ship class

--- ship.py
+++ ship.py
@@ -2,7 +2,7 @@ import pygame
 
 class Ship:
     '''A class to manage the ship'''
-    def __init__(self):
+    def __init__(self, ai_game):
         '''Initialize the ship and set it's starting position'''
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
