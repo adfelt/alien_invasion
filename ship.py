3ed364ba75c161e15fa17a2e88e2162d2fad7020
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-02-17 22:59:38 -0800

    Fix: Added missing attribute to ship class

--- ship.py
+++ ship.py
@@ -5,7 +5,8 @@ class Ship:
     def __init__(self, ai_game):
         '''Initialize the ship and set it's starting position'''
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.settings
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
