6d5bbc8784a92daff3b77d87352dd858b038b50e
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-03-08 22:03:10 -0800

    Feat: Reset the game after pressing play

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,7 +72,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         '''Start a new game when the player clicks play.'''
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaingin aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
 # Keyboard Methods
     def _check_keydown_events(self,event):
