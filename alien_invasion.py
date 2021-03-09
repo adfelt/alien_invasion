cfea1fa8a839e3f3d216a13c5de9802daac3369a
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-03-08 22:11:15 -0800

    Feat: Made cursor invisible during play

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,6 +73,9 @@ class AlienInvasion:
         '''Start a new game when the player clicks play.'''
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -84,6 +87,7 @@ class AlienInvasion:
             # Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
+
 
 # Keyboard Methods
     def _check_keydown_events(self,event):
@@ -238,9 +242,10 @@ class AlienInvasion:
 
             #Pause
             sleep(0.5)
-        
+
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
 
 if __name__ =='__main__':
