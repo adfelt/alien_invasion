4c41c0e8e8c9fe26a29eb7159b9da354aad13dd9
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2021-03-16 22:35:46 -0700

    Feat: Updated the score for each collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -148,6 +148,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
                     self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+
         # Create a new fleet when the aliens are all destroyed
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
